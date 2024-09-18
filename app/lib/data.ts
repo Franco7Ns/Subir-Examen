d7e46aac10bd18a0c8d69b2177310cf91ba02484
Franco7Ns <franco.barragan@nearsure.com>
2024-09-18 10:39:52 -0300

    Actualizacion

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,16 +11,15 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
+    // We artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+ 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
+ 
+    console.log('Data fetch completed after 3 seconds.');
+ 
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
@@ -28,6 +27,7 @@ export async function fetchRevenue() {
   }
 }
 
+  
 export async function fetchLatestInvoices() {
   try {
     const data = await sql<LatestInvoiceRaw>`
@@ -156,8 +156,11 @@ export async function fetchInvoiceById(id: string) {
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
+
+      
     }));
 
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
