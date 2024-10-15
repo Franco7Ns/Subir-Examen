0c8affcc4889602cbcd8ce1c6897fd56de465d40
Franco7Ns <franco.barragan@nearsure.com>
2024-10-15 15:04:31 -0300

    Actualizacion

--- app/alumno/page.tsx
+++ app/alumno/page.tsx
@@ -12,6 +12,12 @@ interface Nota {
   calificacion: number;
 }
 
+interface Users {
+  id: string;
+  name: string;
+  email: string;
+}
+
 export default async function ExamList() {
   const res = await sql`
     SELECT id, name, subject, date
@@ -23,10 +29,22 @@ export default async function ExamList() {
     FROM notas, alumnos
     WHERE alumno_id = alumnos.id
   `;
+
+  const user = await sql`
+    SELECT name, email
+    FROM users
+    WHERE email = 'alumno2@gmail.com'
+  `;
   
   const notas: Nota[] = not.rows.map((row) => ({
     id: row.id,
     calificacion: row.calificacion,
+  }));
+
+  const users: Users[] = user.rows.map((row) => ({
+    id: row.id,
+    name: row.name,
+    email: row.email,
   }));
 
   const examenes: Exam[] = res.rows.map((row) => ({
@@ -54,6 +72,17 @@ export default async function ExamList() {
         {notas.map((nota) => (
           <div key={nota.id} className="bg-white p-6 rounded-lg shadow-md border border-green-200">
             <h3 className="text-lg font-semibold text-green-800 mb-2">{nota.calificacion}</h3>
+            <p className="text-green-700 mb-2">
+            </p>
+            <p className="text-green-600 mb-4">Materia: </p>
+            <div className="flex justify-between">
+            </div>
+          </div>
+        ))}
+        {users.map((user) => (
+          <div key={user.id} className="bg-white p-6 rounded-lg shadow-md border border-green-200">
+            <h3 className="text-lg font-semibold text-green-800 mb-2">{user.name}</h3>
+            <h3 className="text-lg font-semibold text-green-800 mb-2">{user.email}</h3>
             <p className="text-green-700 mb-2">
             </p>
             <p className="text-green-600 mb-4">Materia: </p>
